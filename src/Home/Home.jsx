7a4f812e3fd29639ac15a3a258f42353b7fa6e88
Amzad100 <mdamzad2003a@gmail.com>
2023-04-12 01:07:24 +0600

    job category added

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
             </div>
             {/*banner section end*/}
 
-            {/* job catagory section */}
+            {/* job catagory section start*/}
             <div className='my-5'>
                 <h1>Job Category List</h1>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
@@ -40,6 +40,8 @@ const Home = () => {
                     </div>)
                 }
             </div>
+            {/* job catagory section end*/}
+
             <Featured></Featured>
         </>
     );
