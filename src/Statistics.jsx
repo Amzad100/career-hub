aa6cd77c913c3613aa4f1709237b8924a9df6f1c
Amzad100 <mdamzad2003a@gmail.com>
2023-04-12 17:48:08 +0600

    bar chart added

--- src/Statistics.jsx
+++ src/Statistics.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 const Statistics = () => {
+    const assaignmentMarks = [
+        { name: "Assaignment 1", marks: 60 },
+        { name: "Assaignment 2", marks: 60 },
+        { name: "Assaignment 3", marks: 60 },
+        { name: "Assaignment 4", marks: 60 },
+        { name: "Assaignment 5", marks: 60 },
+        { name: "Assaignment 6", marks: 44 },
+        { name: "Assaignment 7", marks: 60 },
+        { name: "Assaignment 8", marks: 58 },
+    ]
     return (
         <>
-            <h1>Statistics</h1>
+            <h1>Assaignment marks Bar Chart</h1>
+            <BarChart
+                width={1200}
+                height={300}
+                data={assaignmentMarks}
+            >
+                <Bar dataKey="marks"></Bar>
+                <XAxis dataKey="name"></XAxis>
+                <YAxis></YAxis>
+                <Tooltip></Tooltip>
+            </BarChart>
         </>
     );
 };
